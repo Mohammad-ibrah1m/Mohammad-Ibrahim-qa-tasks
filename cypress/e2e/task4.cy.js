8916da0eea8d33921103f5c04d8917d5716d0a86
Mohammad-ibrah1m <mohammads.ibrahim0@gmail.com>
2023-11-29 18:21:34 +0200

    edited add to cart button click using {force:true}

--- cypress/e2e/task4.cy.js
+++ cypress/e2e/task4.cy.js
@@ -3,12 +3,13 @@
 describe('task4', () => {
   it('Add a medium size purple shirt to cart', () => {
     cy.visit('https://magento.softwaretestingboard.com/');
+    cy.wait(2000);
     cy.get('#search').type('Shirt');
     cy.wait(2000);
     cy.get('.action.search').click();
     cy.get('.product-item:first-child div[option-id="168"]').click();
     cy.get('.product-item:first-child .swatch-option.color:last-child').click();
-    cy.get('.product-item:first-child form[data-role="tocart-form"]').submit();
+    cy.get('.product-item:first-child .tocart').click({ force: true });
     cy.wait(3000);
     cy.get('.action.showcart').click();
     cy.get('.details-qty.qty label+input').clear().type(3);
