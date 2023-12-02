191da063fa657b05e4d15f5b2c608970adc006c9
Mohammad-ibrah1m <mohammads.ibrahim0@gmail.com>
2023-12-02 16:05:31 +0200

    added create an account  test case -Happy scenario-

--- cypress/e2e/task5.cy.js
+++ cypress/e2e/task5.cy.js
@@ -9,5 +9,19 @@ describe('task5', () => {
     cy.get('#password').should('be.visible');
     cy.get('#password-confirmation').should('be.visible');
     cy.get('button.submit').should('be.visible');
+    cy.get('.action.back').should('be.hidden');
+  });
+  it.only('Create an account -Happy scenario-', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/');
+    cy.get('#firstname').clear().type('Mohammad', { delay: 100 });
+    cy.get('#lastname').clear().type('Ibrahim', { delay: 100 });
+    cy.get('#email_address').clear().type('mohammadibrahim9@gmail.com', { delay: 100 });
+    cy.get('#password').clear().type('Mohammad_task5', { delay: 100 });
+    cy.get('#password-confirmation').clear().type('Mohammad_task5', { delay: 100 });
+
+    // cy.get('button.submit').click();
+    //I made this line a comment so that the account doesn't get registered during solving the task and give a failed registeration message
+    // cy.wait(2000);
+    // cy.contains('Thank you for registering with Main Website Store.').should('be.visible'); this should pass after a successful registeration in My Account page
   });
 });
